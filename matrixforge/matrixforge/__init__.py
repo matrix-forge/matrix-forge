92b46f5c10b771d04cd357e020edd72316f4a6d5
Kacper Popek <126706381+Kacperaan@users.noreply.github.com>
2023-05-29 11:07:57 +0200

    Update __init__.py

--- matrixforge/matrixforge/__init__.py
+++ matrixforge/matrixforge/__init__.py
@@ -1,1 +1,10 @@
+'''
+MatrixForge
+===========
+Copyright (C) 2023 Kacper Popek All rights reserved 
+FOR MORE INFORMATIONS READ LICENCE
+'''
 
+from mf.activation import *
+from mf.layer import *
+from mf.visualize import *
